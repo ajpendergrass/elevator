a04ce63504981858d803ca9ec8f6ca5c51e5543d
ajpendergrass <ajpendergrass@gmail.com>
2019-11-06 11:58:30 -0500

    commit

--- elevator.js
+++ elevator.js
@@ -1,4 +1,6 @@
 
+var noFloors = 34;
+
 var Elevator = function (elevatorNo, active, direction, currentFloor, occupied, tripCount, floorsPassed) {
     this.elevatorNo = elevatorNo;
     this.active = active;
@@ -7,22 +9,22 @@ var Elevator = function (elevatorNo, active, direction, currentFloor, occupied, 
     this.occupied = occupied;
     this.tripCount = tripCount;
     this.floorsPassed = floorsPassed;
-    this.elevatorMovt = function () {}
-        while (this.direction === 0 && this.currentFloor <= noFloors) {
-            this.currentFloor +=1;
+    this.elevatorMovt = function () {
+        while (this.direction === 0 && this.currentFloor <= 34) {
+            this.currentFloor = this.currentFloor + 1;
             return this.currentFloor;
         }
-    
     }
+}
 
 var ground = 1;
+var noElevators = 3;
 var maxTrips = 100;
-var noFloors = 34;
-var elevator1 = new Elevator ('Elevator1', true, 0, false, 0, 0);
-var elevator2 = new Elevator ('Elevator2', true, 0, false, 0, 0); 
-var elevator3 = new Elevator ('Elevator', true, 0, false, 0, 0);
+var elevator1 = new Elevator ('Elevator1', true, 0, 1, false, 0, 0);
+var elevator2 = new Elevator ('Elevator2', true, 0, 1, false, 0, 0); 
+var elevator3 = new Elevator ('Elevator', true, 0, 1, false, 0, 0);
 
 
 elevator1.elevatorMovt();
 console.log(elevator1);
-
+console.log(noFloors);
