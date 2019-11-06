95cab79304b61d1e08e807de57cc3638ae3d3444
ajpendergrass <ajpendergrass@gmail.com>
2019-11-06 11:47:04 -0500

    commit

---
+++ elevator.js
@@ -0,0 +1,28 @@
+
+var Elevator = function (elevatorNo, active, direction, currentFloor, occupied, tripCount, floorsPassed) {
+    this.elevatorNo = elevatorNo;
+    this.active = active;
+    this.direction = direction;
+    this.currentFloor = currentFloor;
+    this.occupied = occupied;
+    this.tripCount = tripCount;
+    this.floorsPassed = floorsPassed;
+    this.elevatorMovt = function () {}
+        while (this.direction === 0 && this.currentFloor <= noFloors) {
+            this.currentFloor +=1;
+            return this.currentFloor;
+        }
+    
+    }
+
+var ground = 1;
+var maxTrips = 100;
+var noFloors = 34;
+var elevator1 = new Elevator ('Elevator1', true, 0, false, 0, 0);
+var elevator2 = new Elevator ('Elevator2', true, 0, false, 0, 0); 
+var elevator3 = new Elevator ('Elevator', true, 0, false, 0, 0);
+
+
+elevator1.elevatorMovt();
+console.log(elevator1);
+
