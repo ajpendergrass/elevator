33e3e177df7c541c1785f09b69df7be4258f4372
ajpendergrass <ajpendergrass@gmail.com>
2019-11-06 12:02:21 -0500

    commit

--- elevator.js
+++ elevator.js
@@ -1,18 +1,19 @@
 
 var noFloors = 34;
 
-var Elevator = function (elevatorNo, active, direction, currentFloor, occupied, tripCount, floorsPassed) {
+var Elevator = function (elevatorNo, active, direction, openDoors, currentFloor, occupied, tripCount, floorsPassed) {
     this.elevatorNo = elevatorNo;
     this.active = active;
     this.direction = direction;
+    this.openDoors = true;
     this.currentFloor = currentFloor;
     this.occupied = occupied;
     this.tripCount = tripCount;
     this.floorsPassed = floorsPassed;
     this.elevatorMovt = function () {
-        while (this.direction === 0 && this.currentFloor <= 34) {
-            this.currentFloor = this.currentFloor + 1;
-            return this.currentFloor;
+        while (this.active && this.direction === 0 && this.currentFloor <= noFloors) {
+            this.currentFloor +=1;
+            console.log(this.currentFloor);
         }
     }
 }
@@ -20,9 +21,9 @@ var Elevator = function (elevatorNo, active, direction, currentFloor, occupied, 
 var ground = 1;
 var noElevators = 3;
 var maxTrips = 100;
-var elevator1 = new Elevator ('Elevator1', true, 0, 1, false, 0, 0);
-var elevator2 = new Elevator ('Elevator2', true, 0, 1, false, 0, 0); 
-var elevator3 = new Elevator ('Elevator', true, 0, 1, false, 0, 0);
+var elevator1 = new Elevator ('Elevator1', true, 0, 0, 1, false, 0, 0);
+var elevator2 = new Elevator ('Elevator2', true, 0, 0, 1, false, 0, 0); 
+var elevator3 = new Elevator ('Elevator', true, 0, 0, 1, false, 0, 0);
 
 
 elevator1.elevatorMovt();
